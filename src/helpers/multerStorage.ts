dc3ae0f276b0e6bb9e6468bd59751b6d0765850b
ShivharakhYadav <yadavshivharakhyadav@gmail.com>
2023-12-10 14:00:37 +0530

    All

---
+++ src/helpers/multerStorage.ts
@@ -0,0 +1,34 @@
+// require('dotenv').config();
+import multer from "multer";
+const multerStorage = multer.diskStorage({
+    destination: "./src/uploads",
+    filename(req: any, file, callback) {
+        console.log('file-->', file);
+        const userid = req.query.userid;
+
+        //returning imageName
+        const imageName = `${userid}-${Date.now()}-${file.originalname}`;
+        req.uploadTime = Date.now();
+        file.originalname = imageName;
+        callback(null, imageName);
+    },
+})
+export default multerStorage;
+
+// const { MulterError } = require('multer');
+// const multer = require('multer');
+// const multerStorage = multer.diskStorage({
+//     destination: './src/All_Post',
+//     filename: (req, file, cb) => {
+//         // console.log('file', file);
+//         const userid = req.query.userid;
+
+//         //returning imageName
+//         const imageName = `${userid}-${Date.now()}-${file.originalname}`;
+//         req.uploadTime = Date.now();
+//         file.originalname = imageName;
+//         cb(null, imageName);
+//     },
+// });
+
+// module.exports = multerStorage;
